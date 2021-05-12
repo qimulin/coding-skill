08db76b1574a99f37c5ee62a03b7db7003297d34
linxichun <linxichun@citydo.com.cn>
2021-05-12 11:33:16 +0800

    首次提交

---
+++ src/main/java/lin/xc/coding/skill/CodingSkillApplication.java
@@ -0,0 +1,13 @@
+package lin.xc.coding.skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingSkillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lin/xc/coding/skill/CodingSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package lin.xc.coding.skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingSkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
